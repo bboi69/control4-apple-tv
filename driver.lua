fa1e54186dbc332f3ecd0e8c2b230368b903112d
bmrowe <rowe.bryan@gmail.com>
2026-05-03 22:13:21 -0500

    Bump driver version for Pair-Verify diagnostics

--- driver.lua
+++ driver.lua
@@ -20,7 +20,7 @@ require('drivers-common-public.global.timer')
 require('drivers-common-public.global.handlers')
 
 local Driver = {
-  VERSION = "0.1.0-dev",
+  VERSION = "0.1.1-dev",
 }
 
 local function has_c4()
